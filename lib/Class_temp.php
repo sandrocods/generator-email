fa11c54b3cb9dd7e98464aeee4a7d692c4fa4c54
Sandro Putraa <59155826+sandrocods@users.noreply.github.com>
2020-11-01 22:42:37 +0700

    Update Class_temp.php

--- lib/Class_temp.php
+++ lib/Class_temp.php
@@ -3,10 +3,26 @@
  * Generator Email Class Code by sandroputraa
  * https://github.com/sandrocods
  * Created At 31-10-2020
+ * Updated at 01-11-2020
  */
 class GenEmail
 {
     const API = 'https://generator.email/';
+    private static function headers($name = null, $domain = null)
+    {
+        $headers_tempm = array();
+        $headers_tempm[] = 'Host: generator.email';
+        $headers_tempm[] = 'Connection: keep-alive';
+        $headers_tempm[] = 'Upgrade-Insecure-Requests: 1';
+        $headers_tempm[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36';
+        $headers_tempm[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
+        $headers_tempm[] = 'Sec-Fetch-Site: none';
+        $headers_tempm[] = 'Sec-Fetch-Mode: navigate';
+        $headers_tempm[] = 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8';
+        $headers_tempm[] = 'Cookie: _ga=GA1.2.727824364.1578704779; _gid=GA1.2.139870167.1578704779; embx=%5B%22'.$name.'%40'.$domain.'%22%5D; surl='.$domain.'%2F%3F'.$name.'';
+        return $headers_tempm;
+    }
+
     public static function getStr($string, $start, $end)
     {
         $str = explode($start, $string);
@@ -78,16 +94,8 @@ class GenEmail
     }
     public static function ReadSingleMessage($name = null, $domain = null)
     {
-        $headers_tempm = array();
-        $headers_tempm[] = 'Host: generator.email';
-        $headers_tempm[] = 'Connection: keep-alive';
-        $headers_tempm[] = 'Upgrade-Insecure-Requests: 1';
-        $headers_tempm[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36';
-        $headers_tempm[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
-        $headers_tempm[] = 'Sec-Fetch-Site: none';
-        $headers_tempm[] = 'Sec-Fetch-Mode: navigate';
-        $headers_tempm[] = 'Cookie: _ga=GA1.2.727824364.1578704779; _gid=GA1.2.139870167.1578704779; embx=%5B%22'.$name.'%40'.$domain.'%22%5D; surl='.$domain.'%2F%3F'.$name.'';
-        $read_email = GenEmail::curl(self::API, 'GET', null, '1', $headers_tempm);
+        $read_email = GenEmail::curl(self::API, 'GET', null, '1', GenEmail::headers($name ,$domain));
+        return $read_email;
         if (preg_match('/<div class="e7m from_div_45g45gg">(.*?)<\/div>/', $read_email[2], $sender)) {
             return array(
                 "Sender" => $sender[1],
@@ -104,16 +112,7 @@ class GenEmail
     }
     public static function ReadSecret($name = null, $domain = null)
     {
-        $headers_tempm = array();
-        $headers_tempm[] = 'Host: generator.email';
-        $headers_tempm[] = 'Connection: keep-alive';
-        $headers_tempm[] = 'Upgrade-Insecure-Requests: 1';
-        $headers_tempm[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36';
-        $headers_tempm[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
-        $headers_tempm[] = 'Sec-Fetch-Site: none';
-        $headers_tempm[] = 'Sec-Fetch-Mode: navigate';
-        $headers_tempm[] = 'Cookie: _ga=GA1.2.727824364.1578704779; _gid=GA1.2.139870167.1578704779; embx=%5B%22'.$name.'%40'.$domain.'%22%5D; surl='.$domain.'%2F%3F'.$name.'';
-        $ReadMultiMessage = GenEmail::curl(self::API, 'GET', null, '1', $headers_tempm);
+        $ReadMultiMessage = GenEmail::curl(self::API, 'GET', null, '1', GenEmail::headers($name ,$domain));
         if (preg_match('/<div class="e7m from_div_45g45gg">(.*?)<\/div>/', $ReadMultiMessage[2], $sender)) {
             preg_match('/<span id="mess_number">(.*?)<\/span>/', $ReadMultiMessage[2], $output_counter);
             preg_match_all('/<a href="\/'.$domain.'\/'.$name.'\/(.*?)"/',$ReadMultiMessage[2],$output_secret);
@@ -129,15 +128,10 @@ class GenEmail
     }
 
     public static function ReadMessagebySecret($name = null, $domain = null, $secret = null)
-    {
+    {   
         $headers_tempm = array();
-        $headers_tempm[] = 'Host: generator.email';
-        $headers_tempm[] = 'Connection: keep-alive';
-        $headers_tempm[] = 'Upgrade-Insecure-Requests: 1';
-        $headers_tempm[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36';
-        $headers_tempm[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
-        $headers_tempm[] = 'Sec-Fetch-Site: none';
-        $headers_tempm[] = 'Sec-Fetch-Mode: navigate';
+        $headers_tempm = GenEmail::headers($name ,$domain);
+        $pop = array_pop($headers_tempm);
         $headers_tempm[] = 'Cookie: _ga=GA1.2.727824364.1578704779; _gid=GA1.2.139870167.1578704779; embx=%5B%22'.$name.'%40'.$domain.'%22%5D; surl='.$domain.'%2F%3F'.$name.'%2F'.$secret;
         $read_email = GenEmail::curl(self::API, 'GET', null, '1', $headers_tempm);
         if (preg_match('/<div class="e7m from_div_45g45gg">(.*?)<\/div>/', $read_email[2], $sender)) {
@@ -156,19 +150,9 @@ class GenEmail
 
     public static function MarkAllRead($name = null, $domain = null)
     {
-        $headers_tempm[] = 'Host: generator.email';
-        $headers_tempm[] = 'Connection: keep-alive';
-        $headers_tempm[] = 'Origin: https://generator.email';
-        $headers_tempm[] = 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8';
-        $headers_tempm[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36';
-        $headers_tempm[] = 'Accept: */*';
-        $headers_tempm[] = 'X-Requested-With: XMLHttpRequest';
-        $headers_tempm[] = 'Sec-Fetch-Site: none';
-        $headers_tempm[] = 'Sec-Fetch-Mode: navigate';
-        $headers_tempm[] = 'Cookie: _ga=GA1.2.590889529.1589712978; __gads=ID=59509b7dd5c0a14f-2224f9122ac40061:T=1602953322:RT=1602953322:S=ALNI_Mbple5MZn9F0YPSlPvEx6f8RBe7mg; _gat=1; _gid=GA1.2.452373007.1604148961; embx=%5B%22'.$name.'%40'.$domain.'%22%5D; surl='.$domain.'%2F'.$name.'';
-        $read_email = GenEmail::curl(self::API, 'GET', null, '1', $headers_tempm);
+        $read_email = GenEmail::curl(self::API, 'GET', null, '1', GenEmail::headers($name ,$domain));
         $Get_dell = GenEmail::getStr($read_email[2],'{ delll: "','" }');
-        $MarkAllRead = GenEmail::curl(self::API.'del_mail.php', 'POST', 'markall='.$Get_dell.'', null, $headers_tempm);
+        $MarkAllRead = GenEmail::curl(self::API.'del_mail.php', 'POST', 'markall='.$Get_dell.'', null, GenEmail::headers($name ,$domain));
         return array(
             'Status' => $MarkAllRead[2] 
         );
@@ -177,26 +161,15 @@ class GenEmail
 
     public static function DeleteAll($name = null, $domain = null)
     {
-        $headers_tempm = array();
-        $headers_tempm[] = 'Host: generator.email';
-        $headers_tempm[] = 'Connection: keep-alive';
-        $headers_tempm[] = 'Origin: https://generator.email';
-        $headers_tempm[] = 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8';
-        $headers_tempm[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36';
-        $headers_tempm[] = 'Accept: */*';
-        $headers_tempm[] = 'X-Requested-With: XMLHttpRequest';
-        $headers_tempm[] = 'Sec-Fetch-Site: none';
-        $headers_tempm[] = 'Sec-Fetch-Mode: navigate';
-        $headers_tempm[] = 'Cookie: _ga=GA1.2.590889529.1589712978; __gads=ID=59509b7dd5c0a14f-2224f9122ac40061:T=1602953322:RT=1602953322:S=ALNI_Mbple5MZn9F0YPSlPvEx6f8RBe7mg; _gat=1; _gid=GA1.2.452373007.1604148961; embx=%5B%22'.$name.'%40'.$domain.'%22%5D; surl='.$domain.'%2F'.$name.'';
-        $read_email = GenEmail::curl(self::API, 'GET', null, '1', $headers_tempm);
+        $read_email = GenEmail::curl(self::API, 'GET', null, '1', GenEmail::headers($name ,$domain));
         $Get_dell = GenEmail::getStr($read_email[2],'{ delll: "','" }');
-        $DeleteAll = GenEmail::curl(self::API.'del_mail.php', 'POST', 'dellall='.$Get_dell.'', null, $headers_tempm);
+        $DeleteAll = GenEmail::curl(self::API.'del_mail.php', 'POST', 'dellall='.$Get_dell.'', null, GenEmail::headers($name ,$domain));
         return array(
             'Status' => $DeleteAll[2] 
         );
     }
 
-    public static function ReadAllMessage($name = null, $domain = null, $secret = null){
+    public static function ReadAllMessage($name = null, $domain = null){
         $Get_secret = GenEmail::ReadSecret($name , $domain);
         foreach ($Get_secret['Secret'] as $secret) {
             $Get_message = GenEmail::ReadMessagebySecret($name , $domain, $secret);
